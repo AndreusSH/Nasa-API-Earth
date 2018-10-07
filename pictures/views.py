bcac8a67a04596f3e04177e06bb74cb9d14bca55
Andrea <andreamammoliti@yahoo.it>
2018-10-07 18:47:49 +0800

    Adding date

--- pictures/views.py
+++ pictures/views.py
@@ -12,12 +12,12 @@ def search(request):
     
     lon = "lon="+longitude  #    lon = "lon=100.75"#+longitude
 
-    lat = "&lat="+latitude #"&lat=1.5"#+latitude
-    date = "&date=2014-02-01"
+    lat = "&lat="+latitude+"&date=" #"&lat=1.5"#+latitude
+    date = search_date #2014-02-01
     cloud = "&cloud_score=True&"
     api_key = "api_key=N3oZKz7mpvVKaUPnJ617Ov3cAeet9JurXRF5pqcx"
 
     #response = requests.get("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=N3oZKz7mpvVKaUPnJ617Ov3cAeet9JurXRF5pqcx")
     response = requests.get(link+lon+lat+date+cloud+api_key)
     data = response.json()
-    return render (request, 'pictures/search.html', {'picture' : data['url'], 'longitude': longitude, 'latitude' : latitude})
+    return render (request, 'pictures/search.html', {'picture' : data['url'], 'longitude': longitude, 'latitude' : latitude, 'date': date})
