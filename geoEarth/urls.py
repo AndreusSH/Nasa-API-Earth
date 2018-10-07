5c7b74cc7712336dcf61025ac4273efcb75b9001
Andrea <andreamammoliti@yahoo.it>
2018-10-07 16:43:24 +0800

    Collect static

--- geoEarth/urls.py
+++ geoEarth/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
 import pictures.views
 
 
@@ -8,4 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', pictures.views.home, name = "home"),
     path('search', pictures.views.search, name = "search")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
