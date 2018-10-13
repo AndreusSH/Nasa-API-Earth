ced811e98f96bc91720f50ae29202fc0f233f1cb
Andrea Mammoliti <andreamammoliti@yahoo.it>
2018-10-13 17:06:06 +0800

    Update views.py

--- pictures/views.py
+++ pictures/views.py
@@ -15,7 +15,7 @@ def search(request):
     lat = "&lat="+latitude+"&date=" #"&lat=1.5"#+latitude
     date = search_date #2014-02-01
     cloud = "&cloud_score=True&"
-    api_key = "api_key=N3oZKz7mpvVKaUPnJ617Ov3cAeet9JurXRF5pqcx"
+    api_key = "get_your_own_Api_following_the_README.md"
 
     #response = requests.get("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=N3oZKz7mpvVKaUPnJ617Ov3cAeet9JurXRF5pqcx")
     response = requests.get(link+lon+lat+date+cloud+api_key)
